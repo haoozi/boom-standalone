458bae5b3031e5626007f152416e58015af52181
Haoyuan Wang <hwang208@ucsc.edu>
2022-09-06 16:31:16 -0700

    Add configs

--- src/BoomConfigs.scala
+++ src/BoomConfigs.scala
@@ -29,11 +29,32 @@ class MegaBoomConfig extends Config(
   new freechips.rocketchip.system.BaseConfig)
 
 class DualSmallBoomConfig extends Config(
-  new boom.common.WithNSmallBooms(2) ++                          // 2 boom cores
+  new boom.common.WithNSmallBooms(2) ++                          // 2 small boom cores
   new WithCoherentBusTopology ++
   new freechips.rocketchip.system.BaseConfig)
 
-class QuadMegaBoomConfig extends Config(
-  new boom.common.WithNMegaBooms(4) ++                          // 4 mega boom cores
+class QuadSmallBoomConfig extends Config(
+  new boom.common.WithNSmallBooms(4) ++                          // 4 small boom cores
   new WithCoherentBusTopology ++
   new freechips.rocketchip.system.BaseConfig)
+
+class DualLargeBoomConfig extends Config(
+   new boom.common.WithNLargeBooms(2) ++                          // 2 large boom cores
+   new WithCoherentBusTopology ++
+   new freechips.rocketchip.system.BaseConfig)
+
+class QuadLargeBoomConfig extends Config(
+   new boom.common.WithNLargeBooms(4) ++                          // 4 large boom cores
+   new WithCoherentBusTopology ++
+   new freechips.rocketchip.system.BaseConfig)
+
+
+class DualMegaBoomConfig extends Config(
+   new boom.common.WithNMegaBooms(2) ++                          // 2 mega boom cores
+   new WithCoherentBusTopology ++
+   new freechips.rocketchip.system.BaseConfig)
+
+class QuadMegaBoomConfig extends Config(
+   new boom.common.WithNMegaBooms(4) ++                          // 4 mega boom cores
+   new WithCoherentBusTopology ++
+   new freechips.rocketchip.system.BaseConfig)
